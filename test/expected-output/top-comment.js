a1ac0ff3f6f7d062c15c95983d229671fb2bd8ba
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-06-21 21:14:06 +0200

    Remove all but the "mapping.json" file

--- test/expected-output/top-comment.js
+++
@@ -1,7 +0,0 @@
-/**
- * This comment should be preserved, and included before the inserted import statements.
- */
-import Component from "@ember/component";
-
-export default Component.extend({
-});
--- test/input/top-comment.js
+++
@@ -1,5 +0,0 @@
-/**
- * This comment should be preserved, and included before the inserted import statements.
- */
-export default Ember.Component.extend({
-});
