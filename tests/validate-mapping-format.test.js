92f44e8265246f136247f7598bad396ce682f950
Robert Jackson <rjackson@linkedin.com>
2018-04-23 17:08:58 -0400

    Add test validating mapping.json format.

---
+++ tests/validate-mapping-format.test.js
@@ -0,0 +1,13 @@
+const mappings = require('../mappings.json');
+
+for (let mapping of mappings) {
+  describe('Validate mapping format', () => {
+    test(`${mapping.global} - ${mapping.module} - ${mapping.export}`, () => {
+      expect(mapping.global).toMatch(/.+/);
+      expect(mapping.module).toMatch(/.+/);
+      expect(mapping.export).toMatch(/.+/);
+      expect(mapping.localName).toMatch(/.+/);
+      expect([true, false]).toContain(mapping.deprecated);
+    });
+  });
+}
